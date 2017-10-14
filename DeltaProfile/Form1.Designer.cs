1cbecf20b7a9e7aac8369675f83cba7a0c430eef
Richard Curtis Huckaby <rhuckaby3@gatech.edu>
2017-10-14 04:27:04 -0400

    Added c# project

---
+++ DeltaProfile/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DeltaProfile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Are you a talker?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(285, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Are you a non-talker?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Are you a sleeper?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(245, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Are you a drinker?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 498);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(297, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Do you have children?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 571);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(240, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Are you a pee-er?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1869, 915);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
